7ff0eaf4429289117cb3132012f7bd9fee5ac9e2
Daniel Vena <deaconvena+git@gmail.com>
2019-10-27 17:05:08 -0400

    Update test

--- plugin.php
+++ plugin.php
@@ -2,10 +2,10 @@
 /**
  * Plugin Name: Image Category Slideshow
  * Plugin URI: https://github.com/cumulus-digital/gutenberg-category_slideshow
- * Description: Display a slideshow of media in a specified category
+ * Description: Display a slideshow of media within a specified category
  * Author: vena
  * Author URI: https://github.com/vena
- * Version: 1.0.0
+ * Version: 1.0.1
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  * GitHub Plugin URI: cumulus-digital/gutenberg-category_slideshow
